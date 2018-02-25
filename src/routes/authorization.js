5137ddb197ecea6227e24862bdc15c84a8611c41
Vadim Sokoltsov <vforvad@gmail.com>
2018-02-25 23:45:55 +0300

    add default redirect to SignIn component

--- src/routes/authorization.js
+++ src/routes/authorization.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, Redirect, Switch } from 'react-router';
 
 import Authorization from '../containers/Authorization/Authorization';
 import SignIn from '../components/Authorization/SignIn/SignIn';
@@ -7,7 +7,10 @@ import SignIn from '../components/Authorization/SignIn/SignIn';
 export default (
   <Route>
     <Authorization>
-      <Route path="/sign_in" component={SignIn} />
+      <Switch>
+        <Route path="/sign-in" component={SignIn} />
+        <Redirect to="/sign-in" />
+      </Switch>
     </Authorization>
   </Route>
 );
