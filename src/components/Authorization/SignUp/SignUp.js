3cfbf82e5c6cc932a201adee98d858541ee6515f
Vadim Sokoltsov <vforvad@gmail.com>
2018-02-26 01:09:48 +0300

    add SignUp component

---
+++ src/components/Authorization/SignUp/SignUp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+  render() {
+    return (
+      <div>
+        SignUp Component
+      </div>
+    );
+  }
+}
+
+export default SignUp;
--- src/routes/authorization.js
+++ src/routes/authorization.js
@@ -3,12 +3,14 @@ import { Route, Redirect, Switch } from 'react-router';
 
 import Authorization from '../containers/Authorization/Authorization';
 import SignIn from '../components/Authorization/SignIn/SignIn';
+import SignUp from '../components/Authorization/SignUp/SignUp';
 
 export default (
   <Route>
     <Authorization>
       <Switch>
         <Route path="/sign-in" component={SignIn} />
+        <Route path="/sign-up" component={SignUp} />
         <Redirect to="/sign-in" />
       </Switch>
     </Authorization>
