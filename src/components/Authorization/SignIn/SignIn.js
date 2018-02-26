f33ea5ccfc05b1dcce417e402af88f0b4b799c1e
Vadim Sokoltsov <vforvad@gmail.com>
2018-02-27 00:01:15 +0300

    implement sign in component imposition

--- src/components/Authorization/SignIn/SignIn.js
+++ src/components/Authorization/SignIn/SignIn.js
@@ -1,11 +1,54 @@
+import CN from 'classnames';
+
 import React, { Component } from 'react';
 
 class SignIn extends Component {
+
+  state = {
+    email: '',
+    password: ''
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
+    const { email, password } = this.state;
+
     return (
-      <div>
-        SignIn Component
-      </div>
+      <form className={CN('form', 'vertical')} onSubmit={this.handleSubmit}>
+        <div className="form-field">
+          <input
+            type="text"
+            className="field"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+            placeholder="Email" />
+        </div>
+        <div className="form-field">
+          <input
+            type="password"
+            className="field"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+            placeholder="Password" />
+        </div>
+        <div className="form-actions">
+          <button type="submit" className={CN('button', 'success')}>Submit</button>
+        </div>
+      </form>
     );
   }
 }
